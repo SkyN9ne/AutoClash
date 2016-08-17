14a86825fdf5e662eccdf7c89860476e352dc5e1
MicroBlaster <dmc@it1.biz>
2016-08-17 03:09:23 -0500

    Update README.md

---
+++ Source/ADB/Client.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace ADB
+{
+    class Client
+    {
+        /// <summary>
+        /// Flag indicating if the client is connected to ADB.
+        /// </summary>
+        bool connected;
+        public bool Connected { get; set; }
+
+        /// <summary>
+        /// Flag indicating if the last command was successful.
+        /// </summary>
+        bool success;
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Socket for ADB connection.
+        /// </summary>
+        private StreamSocket socket;
+        public StreamSocket Socket { get; set; }
+
+
+        /// <summary>
+        /// Initializes an instance of the ADB client.
+        /// </summary>
+        public Client()
+        {
+            connected = false;
+            socket = new StreamSocket();
+        }
+
+        /// <summary>
+        /// Initializes client connection and starts ADB server if necessary.
+        /// </summary>
+        public async Task<bool> Connect()
+        {
+            if (!connected)
+            {
+                try
+                {
+                    HostName host = new HostName("localhost");
+                    await socket.ConnectAsync(host, "5037");
+                    connected = true;
+                    return true;
+                }
+                catch
+                {
+                    // TODO: start ADB server and retry connection;
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Closes client connection.
+        /// </summary>
+        public void Close()
+        {
+            socket.Dispose();
+            socket = null;
+        }
+
+
+
+        /// <summary>
+        /// Sends a command to the ADB server.
+        /// </summary>
+        /// <param name="command">ADB command string.</param>
+        /// <returns>Result string from ADB command.</returns>
+        public async Task<string> Command(string command)
+        {
+            Task.Delay(1000).Wait();
+
+            if (connected)
+            {
+                try
+                {
+                    // Format the command for ADB server.
+                    string sendData = string.Format("{0}{1}\n", command.Length.ToString("X4"), command);
+ 
+                    // Send the command.
+                    DataWriter writer = new DataWriter(socket.OutputStream);
+                    writer.WriteString(sendData);
+                    await writer.StoreAsync();
+
+                    // Create the data reader.
+                    DataReader reader = new DataReader(socket.InputStream);
+                    reader.InputStreamOptions = InputStreamOptions.Partial;
+                    uint bytesRead = await reader.LoadAsync(1024 * 20);
+
+
+                    if(bytesRead > 3)
+                    {
+                        // Get the status response ('OKAY' or 'FAIL').
+                        String status = reader.ReadString(4);
+                        bytesRead -= 4;
+
+                        if (status == "OKAY")
+                        {
+                            success = true;
+                        }
+                        else
+                        {
+                            success = false;
+                        }
+
+                        if (bytesRead > 3)
+                        {
+                            // todo read propper X4 and convert to length
+                            String length = reader.ReadString(4);
+                            bytesRead -= 4;
+                        }
+
+                    }
+                    else
+                    {
+                        success = false;
+                    }
+
+                    // Return the result string.
+                    return reader.ReadString(bytesRead);
+                }
+                catch
+                {
+                    // TODO: Log error;
+
+                    return "";
+                }
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+
+    }
+}
--- Source/ADB/Device.cs
+++ Source/ADB/Device.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ADB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,87 @@ using System.Threading.Tasks;
 
 namespace ADB
 {
-    public class Device
+    public static class Devices
     {
-        private string Name;
+        static Client client = new Client();
 
-
-        Device()
+        public static async Task<List<Device>> List()
         {
-            Name = "";
+            List<Device> devices = new List<Device>();
+
+            if(await client.Connect())
+            {
+                string result = await client.Command("host:devices-l");
+                client.Close();
+
+                string[] lines = result.Split('\n');
+
+                foreach (string line in lines)
+                {
+                    int pos = line.IndexOf(' ');
+
+                    if (pos > 0)
+                    {
+                        string name = "";
+                        string serial = line.Substring(0, pos);
+                        string description = line.Substring(pos).TrimStart();
+                        devices.Add(new Device(name, serial, description));
+                    }
+                    //else
+                    //{
+                    //    string name = "";
+                    //    string serial = line;
+                    //    string description = "";
+                    //    devices.Add(new Device(name, serial, description));
+                    //}
+
+                }
+
+
+                // Build the device list.
+                return devices;
+            }
+            else
+            {
+                // Unable to get a list of devices.
+                return null;
+            }
+
+
         }
     }
+
+    public class Device
+    {
+        Client client = new Client();
+
+        private string name;
+        private string serial;
+        private string description;
+
+
+        public Device(string Name, string Serial, string Description)
+        {
+            name = Name;
+            serial = Serial;
+            description = Description;
+        }
+
+        public async Task StartClash()
+        {
+            //await client.Connect();
+            //string result2 = await client.Command("host:transport:127.0.0.1:21633"); // MEmu_13
+            //string result2 = await client.Command("host:transport:127.0.0.1:62001"); // Nox
+            //string result2 = await client.Command("host:transport:d230ab45"); // Galaxy s7
+            //string result3 = await client.Command("shell:dumpsys account");
+
+            if (await client.Connect())
+            {
+                string result = await client.Command(string.Format("host:transport:{0}", serial));
+                string result2 = await client.Command("shell:am start -W com.supercell.clashofclans/.GameApp");
+                client.Close();
+            }
+        }
+    }
+
 }
