332ae067fcf2f9f1441be27811dfe6f0a1fbef36
David McCartney <dmc@it1.biz>
2016-08-17 00:41:35 -0500

    Initial Commit

---
+++ Source/ADB/Device.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADB
+{
+    public class Device
+    {
+        private string Name;
+
+
+        Device()
+        {
+            Name = "";
+        }
+    }
+}
